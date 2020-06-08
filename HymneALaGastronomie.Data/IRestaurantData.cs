af0d362c10f90143d5fded5444bb821556747c8c
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-06-09 01:44:40 +0200

    Adding the NotFound page

--- HymneALaGastronomie.Data/IRestaurantData.cs
+++ HymneALaGastronomie.Data/IRestaurantData.cs
@@ -8,6 +8,7 @@ namespace HymneALaGastronomie.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetRestaurantById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -25,12 +26,18 @@ namespace HymneALaGastronomie.Data
                 new Restaurant { Id = 5, Name = "Le Cèdre du Liban", Location = "Namur", Cuisine = CuisineType.Lebanese}
             };
         }
+
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
+        }
+
+        public Restaurant GetRestaurantById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
         }
     }
 }
---
+++ HymneALaGastronomie/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HymneALaGastronomie.CoreBusiness;
+using HymneALaGastronomie.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HymneALaGastronomie.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetRestaurantById(restaurantId);
+
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
