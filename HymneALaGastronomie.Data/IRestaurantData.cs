7a23863409da2cc23304801326a8e25e8561d887
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-06-08 14:57:42 +0200

    Adding the interface IRestaurantData

---
+++ HymneALaGastronomie.Data/IRestaurantData.cs
@@ -0,0 +1,12 @@
+﻿using HymneALaGastronomie.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HymneALaGastronomie.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAllRestaurants();
+    }
+}
