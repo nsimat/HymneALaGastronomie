6b56b3f8fe126bd1acfaa290069bcd7a73928726
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-06-08 20:33:14 +0200

    Adding the font-awesome library

--- HymneALaGastronomie/Pages/Restaurants/List.cshtml.cs
+++ HymneALaGastronomie/Pages/Restaurants/List.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HymneALaGastronomie.CoreBusiness;
+using HymneALaGastronomie.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -11,17 +13,21 @@ namespace HymneALaGastronomie.Pages.Restaurants
     public class ListModel : PageModel
     {
         private readonly IConfiguration configuration;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration configuration)
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
         {
             this.configuration = configuration;
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = configuration["Message"];
+            Restaurants = restaurantData.GetAllRestaurants();
         }
     }
 }
