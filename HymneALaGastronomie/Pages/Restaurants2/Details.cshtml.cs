3159fe68f4e412069140b189cbb67b3a5fbd8aa6
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-06-17 00:44:38 +0200

    Adding a Scaffolding CRUD Razor Restaurant pages

---
+++ HymneALaGastronomie/Pages/Restaurants2/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HymneALaGastronomie.CoreBusiness;
+using HymneALaGastronomie.Data;
+
+namespace HymneALaGastronomie.Pages.Restaurants2
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly HymneALaGastronomie.Data.HymneALaGastronomieDbContext _context;
+
+        public DetailsModel(HymneALaGastronomie.Data.HymneALaGastronomieDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Restaurant = await _context.Restaurants.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Restaurant == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ HymneALaGastronomie/Pages/Restaurants2/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HymneALaGastronomie.CoreBusiness;
+using HymneALaGastronomie.Data;
+
+namespace HymneALaGastronomie.Pages.Restaurants2
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HymneALaGastronomie.Data.HymneALaGastronomieDbContext _context;
+
+        public IndexModel(HymneALaGastronomie.Data.HymneALaGastronomieDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
